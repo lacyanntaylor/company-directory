44654479a8b2833d93282254ef2e8679d51eb0c3
lacyanntaylor <lacy.ann.taylor@gmail.com>
2024-12-15 21:14:12 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,16 @@
+INSERT INTO department (department_name)
+VALUES ('Accounting'),
+        ('Engineering'),
+        ('Human Resources'),
+        ('IT'),
+        ('Marketing'),
+        ('Customer Services'),
+        ('Operations'),
+        ('Sales')
+
+INSERT INTO department_id ()
+
+INSERT INTO employees_id (employees)
+VALUES ('Human Resources', 'supervisor', 'Kathy Henderson')
+        ( 'Abe Johnson', 'Accountant', 80000, 'Accounting',  )
+
